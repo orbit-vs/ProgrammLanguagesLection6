57375bdcf7f9ffbc8f5738af3da5f90ee4ead066
Артем Белоколенко <artembelokolenko@MacBook-Air-Artem.local>
2023-02-09 18:54:03 +0300

    Lection5/

---
+++ Lection5/Program.cs
@@ -0,0 +1,9 @@
+﻿string path = "/Users/artembelokolenko/Documents/учеба/Examples";
+DirectoryInfo di = new DirectoryInfo(path);
+System.Console.WriteLine(di.CreationTime);
+FileInfo[] fi = di.GetFiles();
+
+for (int i = 0; i < fi.Length; i++)
+{
+    System.Console.WriteLine(fi[i].Name);
+}
