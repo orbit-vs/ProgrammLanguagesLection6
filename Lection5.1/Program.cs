4d77c825e3bf50f61334c3fe8eec57d41cd60e66
Артем Белоколенко <artembelokolenko@MacBook-Air-Artem.local>
2023-02-09 18:54:30 +0300

    Lection5.2/

---
+++ Lection5.1/Program.cs
@@ -0,0 +1,19 @@
+﻿void CatalogInfo(string path, string indent = "")
+{
+    DirectoryInfo catalog = new DirectoryInfo(path);
+
+    DirectoryInfo[] catalogs = catalog.GetDirectories();
+    for (int i = 0; i < catalogs.Length; i++)
+    {
+        Console.WriteLine($"{indent}{catalogs[i].Name}");
+        CatalogInfo(catalogs[i].FullName, indent + " ");
+    }
+    FileInfo[] files = catalog.GetFiles();
+
+    for (int i = 0; i < files.Length; i++)
+    {
+        Console.WriteLine($"{indent}{files[i].Name}");
+    } 
+}
+string path = @"Users/artembelokolenko/Documents/учеба/";
+CatalogInfo(path);
